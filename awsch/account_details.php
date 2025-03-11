970b56eb8aa1e6bd34d72577b1181f795ec0da1b
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-03-11 12:36:15 +0500

    Changes

--- awsch/account_details.php
+++ awsch/account_details.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+date_default_timezone_set('Asia/Karachi');
 require '../db.php';
 require '../aws/aws-autoloader.php';
 
