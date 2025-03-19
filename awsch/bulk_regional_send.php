89892b40f76209dcfd10966a1167dd9a6087ae24
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-03-19 12:23:57 +0500

    Changes

--- awsch/bulk_regional_send.php
+++ awsch/bulk_regional_send.php
@@ -86,14 +86,43 @@ if (isset($_GET['stream'])) {
 
   sendSSE("STATUS", "Starting Bulk Regional OTP Process for Set ID: " . $set_id);
 
-  $regions = array(
-    "me-central-1",
-    "ap-southeast-3",
-    "ap-southeast-4",
-    "eu-south-2",
-    "eu-central-2",
-    "ap-south-2"
-  );
+  // Determine regions to process based on GET parameter 'region'
+  if (isset($_GET['region']) && !empty($_GET['region'])) {
+      // Process the specified region only.
+      $regions = array($_GET['region']);
+  } else {
+      // Process all regions (using the full list from the select options)
+      $regions = array(
+          "us-east-1",
+          "us-east-2",
+          "us-west-1",
+          "us-west-2",
+          "ap-south-1",
+          "ap-northeast-3",
+          "ap-southeast-1",
+          "ap-southeast-2",
+          "ap-northeast-1",
+          "ca-central-1",
+          "eu-central-1",
+          "eu-west-1",
+          "eu-west-2",
+          "eu-west-3",
+          "eu-north-1",
+          "me-central-1",
+          "sa-east-1",
+          "af-south-1",
+          "ap-southeast-3",
+          "ap-southeast-4",
+          "ca-west-1",
+          "eu-south-1",
+          "eu-south-2",
+          "eu-central-2",
+          "me-south-1",
+          "il-central-1",
+          "ap-south-2"
+      );
+  }
+  
   $totalRegions = count($regions);
   $totalSuccess = 0;
   $usedRegions = 0;
