8671889d044e49fbba9af7988f2ab863ef4c779e
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-05-07 17:47:27 +0500

    Changes

--- awsch/child_actions/scan_instances.php
+++ awsch/child_actions/scan_instances.php
@@ -1,3 +1,4 @@
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -90,3 +91,4 @@ try {
     http_response_code(500);
     echo json_encode(['error' => 'Error: ' . $e->getMessage()]);
 }
+?>
