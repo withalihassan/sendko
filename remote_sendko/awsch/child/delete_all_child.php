de39761c6ac0900366dfa2d432b249f7749ae354
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-05-14 10:01:14 +0500

    Changes

--- remote_sendko/awsch/child/delete_all_child.php
+++ remote_sendko/awsch/child/delete_all_child.php
@@ -1,16 +1,19 @@
 <?php
-require '../db_connect.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require '../../db.php';
 $accountId = $_GET['parent_id'];
 
 // Prepare and execute the DELETE query based on parent_account_id
-$query = "DELETE  FROM child_accounts WHERE parent_id = '$accountId'";
+$query = "DELETE FROM child_accounts WHERE parent_id = '$accountId'";
 
-if ($conn->query($query) === TRUE) {
+if ($pdo->query($query) === TRUE) {
     echo "
        <script>
         setTimeout(function() { window.location.href = 'new_page.php'; }, 1000); 
         </script>
        ";
 } else {
-    $message = "Error deleting child accounts: " . $conn->error;
+    $message = "Error deleting child accounts: ";
 }
