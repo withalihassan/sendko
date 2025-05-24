42b4954f3f9cf9488d2727ecc767c937751c9fb4
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-05-24 07:17:47 +0500

    Changes

--- remote_sendko/db.php
+++ remote_sendko/db.php
@@ -2,7 +2,7 @@
 // db.php
 
 // Determine the database host based on the URL used to access the site.
-if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === '47.89.232.110') {
+if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === '18.206.99.184s') {
     // When accessed via http://47.251.28.20, use 'localhost' for the DB connection.
     $host = 'localhost';
 } else {
