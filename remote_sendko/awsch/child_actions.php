3e58a1e55e7303a2cb6734b5ffd29e456b077db5
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2025-12-15 07:00:21 +0500

    all 15dec

--- remote_sendko/awsch/child_actions.php
+++ remote_sendko/awsch/child_actions.php
@@ -169,8 +169,8 @@ try {
         <div class="row mb-2">
             <div class="col-md-2">
                 <select id="regionSelect" class="form-select">
-                    <option value="us-east-1">US East (N. Virginia)</option>
                     <option value="us-east-2">US East (Ohio)</option>
+                    <option value="us-east-1">US East (N. Virginia)</option>
                     <option value="us-west-1">US West (N. California)</option>
                     <option value="us-west-2">US West (Oregon)</option>
                     <hr>
@@ -193,10 +193,10 @@ try {
             </div>
             <div class="col-md-2">
                 <select id="instanceType" class="form-select">
+                    <option value="t2.micro">t2.micro</option>
                     <option value="c7a.4xlarge">C7 32v</option>
                     <option value="c7a.2xlarge">c7a 8V</option>
                     <option value="c7a.xlarge">C7 4v</option>
-                    <option value="t2.micro">t2.micro</option>
                     <option value="c5a.xlarge">c5a.xlarge</option>
                     <option value="c7a.8xlarge">c7a.8xlarge</option>
                     <option value="c7i.xlarge">c7i.xlarge</option>
