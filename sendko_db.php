73eaabdf5d0254c43fe967177f78a8af24e6fe1b
withalihassan <macbookpro@Alis-MacBook-Pro.local>
2026-03-03 10:26:02 +0500

    all 2 march

--- sendko_db.php
+++ sendko_db.php
@@ -1,26 +1,28 @@
 <?php
 // sendko_db.php
 
-function openSendkkoConnection() {
+function openSendkkoConnection()
+{
     if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === '13.220.207.140') {
         $host = 'localhost';
     } else {
-        $host = 'database-1.ct22ws4u0c7g.me-central-1.rds.amazonaws.com';
+        $host = 'database-1.cjiuwqmaw256.ap-south-1.rds.amazonaws.com';
     }
 
     $dbname   = 'sender';
     $username = 'admin';
-    $password = 'sLoGMCVfEo4TpMGOEm18';
+    $password = '3CFz8no5NSxCXiDOMz8g';
 
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
-    } catch(PDOException $e) {
+    } catch (PDOException $e) {
         die("Sendkko DB connection failed: " . $e->getMessage());
     }
 }
 
-function closeSendkkoConnection(&$pdo) {
+function closeSendkkoConnection(&$pdo)
+{
     $pdo = null;
 }
